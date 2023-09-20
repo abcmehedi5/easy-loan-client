d97a3013b1ff22c6d17576dd62b6dd6a3c6bbe2b
abcmehedi5 <abcmehedi5@gmail.com>
2023-09-20 16:36:47 +0600

    production bug fixed

--- src/Components/LazyLoader/LazyLoader.jsx
+++ src/Components/LazyLoader/LazyLoader.jsx
@@ -1,7 +1,7 @@
 const LazyLoader = () => {
   return (
     <div className="h-screen flex justify-center items-center">
-      <img src="https://i.ibb.co/d2DhTfv/loading-fast.gif" alt="" />
+      <img src="https://i.ibb.co/nwhhm23/1c13f3fe7a6bba370007aea254e195e3.gif" alt="Loading image" />
     </div>
   );
 };
